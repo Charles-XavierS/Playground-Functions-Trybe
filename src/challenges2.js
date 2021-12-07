d4b3e6f125a0bea969924027fa0a201ff539941a
Charles Xavier <charlesxsaldanha@hotmail.com>
2021-12-07 14:23:22 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,8 +3,8 @@ function techList(arrayNomes, name) {
   let listaObjetos = [];
   arrayNomes.sort(); // ordenar a lista
 
-  for (let index = 0; index < arrayNomes.length; index += 1) {
-    listaObjetos.push({ tech: arrayNomes[index], name });
+  for (let j = 0; j < arrayNomes.length; j += 1) {
+    listaObjetos.push({ tech: arrayNomes[j], name });
   }
 
   if (listaObjetos.length > 0) {
@@ -14,8 +14,25 @@ function techList(arrayNomes, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // gerar os numeros
+function generatePhoneNumber(arrayNumeros) {
+  if (arrayNumeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let i = 0; i < arrayNumeros.length; i += 1) {
+    for (let j = 1; j < arrayNumeros.length; j += 1) {
+      if ([i] === [j]) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+
+  for (let numeros of arrayNumeros) {
+    if (numeros < 0 || numeros > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${arrayNumeros[0]}${arrayNumeros[1]}) ${arrayNumeros[2]}${arrayNumeros[3]}${arrayNumeros[4]}${arrayNumeros[5]}${arrayNumeros[6]}-${arrayNumeros[7]}${arrayNumeros[8]}${arrayNumeros[9]}${arrayNumeros[10]}`
 }
 
 // Desafio 12
